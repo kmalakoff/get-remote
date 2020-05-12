1bc12f05aba2301516b04cd08689ea8ed531ecd9
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-12 07:15:26 -0700

    Prepare for release

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var both = require('./lib/both');
+var downloadOnly = require('./lib/download');
 
-module.exports = function downloadDecompress(src, dest, options, callback) {
+function download(src, dest, options, callback) {
   if (typeof options === 'function') {
     callback = options;
     options = null;
   }
-  both(src, dest, options || {}, callback);
-};
+  options = options || {};
+  if (typeof callback === 'function') {
+    return options.extract ? both(src, dest, options, callback) : downloadOnly(src, dest, options, callback);
+  }
+  return new Promise(function (resolve, reject) {
+    download(src, dest, options, callback, function downloadCallback(err, res) {
+      err ? reject(err) : resolve(res);
+    });
+  });
+}
+
+module.exports = download;
--- test/spec/download.test.js
+++ test/spec/download.test.js
@@ -3,64 +3,31 @@ var path = require('path');
 var fs = require('fs');
 var rimraf = require('rimraf');
 
-var access = require('../lib/access');
+var download = require('../..');
 
 var TMP_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp'));
 
 describe('download', function () {
   beforeEach(rimraf.bind(null, TMP_DIR));
-  // after(rimraf.bind(null, TMP_DIR));
+  after(rimraf.bind(null, TMP_DIR));
 
-  it('should download over http', function (done) {
-    var download = require('../..');
-
+  it('should download over https', function (done) {
     var fullPath = path.join(TMP_DIR, 'node-tests-data');
-    //https://raw.githubusercontent.com/jchip/nvm/v1.3.1/install.ps1
-    download('https://codeload.github.com/kmalakoff/node-tests-data/zip/v1.0.0', fullPath, function (err) {
+    download('https://codeload.github.com/kmalakoff/node-tests-data/zip/v1.0.0', fullPath, { extract: true, strip: 1 }, function (err) {
       assert.ok(!err);
-      access(fullPath, function (err) {
-        assert.ok(!err);
-        done();
-      });
+      var files = fs.readdirSync(fullPath);
+      assert.ok(files.length > 1);
+      done();
     });
   });
 
-  // it('should download over http', function (done) {
-  //   var Download = require('download');
-  //   // var progress = require('download-status');
-
-  //   var fullPath = path.join(TMP_DIR, 'node-tests-data');
-  //   fs.mkdirSync(TMP_DIR);
-
-  //   new Download({ extract: true })
-  //     .get('https://codeload.github.com/kmalakoff/node-tests-data/zip/v1.0.0')
-  //     .dest(TMP_DIR)
-  //     .run(function (err, files) {
-  //       assert.ok(!err);
-  //       access(fullPath, function (err) {
-  //         assert.ok(!err);
-  //         done();
-  //       });
-  //     });
-
-  // var download = new Download()
-  //   .get('https://codeload.github.com/kmalakoff/node-tests-data/zip/v1.0.0', fullPath, { extract: true, strip: 1 })
-  //   .use(progress());
-
-  // download.run(function (err, files) {
-  //   assert.ok(!err);
-
-  //   console.log(files);
-  //   done();
-  //   //=> [{ url: http://example.com/foo.zip, contents: <Buffer 50 4b 03 ...> }, { ... }]
-  // });
-  // //https://raw.githubusercontent.com/jchip/nvm/v1.3.1/install.ps1
-  // download('', fullPath, function (err) {
-  //   assert.ok(!err);
-  //   access(fullPath, function (err) {
-  //     assert.ok(!err);
-  //     done();
-  //   });
-  // });
-  // });
+  it('should download over https', function (done) {
+    var fullPath = path.join(TMP_DIR, 'install.ps1');
+    download('https://raw.githubusercontent.com/jchip/nvm/v1.3.1/install.ps1', fullPath, function (err) {
+      assert.ok(!err);
+      var files = fs.readdirSync(TMP_DIR);
+      assert.ok(files.length === 1);
+      done();
+    });
+  });
 });
