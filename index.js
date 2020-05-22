904736ade896c61f37f92162670bd91c1b3bfbab
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-22 09:38:13 -0700

    More streaming

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-var both = require('./lib/both');
-var downloadOnly = require('./lib/download');
+var download = require('./lib/download');
 
-function download(src, dest, options, callback) {
+function getRemote(src, dest, options, callback) {
   if (typeof options === 'function') {
     callback = options;
     options = null;
   }
   options = options || {};
-  if (typeof callback === 'function') {
-    return options.extract ? both(src, dest, options, callback) : downloadOnly(src, dest, options, callback);
-  }
+  if (typeof callback === 'function') return download(src, dest, options, callback);
   return new Promise(function (resolve, reject) {
-    download(src, dest, options, callback, function downloadCallback(err, res) {
+    getRemote(src, dest, options, function (err, res) {
       err ? reject(err) : resolve(res);
     });
   });
 }
 
-module.exports = download;
+module.exports = getRemote;
