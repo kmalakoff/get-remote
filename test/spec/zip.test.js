9781c730c8f8ec37c5dc8418588e07be7d8d6939
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-22 12:00:48 -0700

    Fixed api interference

---
+++ test/spec/zip.test.js
@@ -0,0 +1,78 @@
+var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp-classic');
+var progressStream = require('progress-stream');
+var unzip = require('node-unzip-2');
+var fstream = require('fstream');
+var semver = require('semver');
+
+var download = require('../..');
+
+var TMP_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp'));
+
+describe('zip', function () {
+  if (semver.lt(process.versions.node, 'v0.10.0')) return;
+
+  beforeEach(function (done) {
+    rimraf(TMP_DIR, function () {
+      mkdirp(TMP_DIR, done);
+    });
+  });
+
+  it('should download unzip over https', function (done) {
+    var dest = [unzip.Parse(), fstream.Writer(TMP_DIR)];
+    download('https://codeload.github.com/kmalakoff/get-remote/zip/0.2.1', dest, function (err) {
+      assert.ok(!err);
+      var files = fs.readdirSync(path.join(TMP_DIR, 'get-remote-0.2.1'));
+      assert.ok(files.length > 1);
+      done();
+    });
+  });
+
+  it('should download unzip over http', function (done) {
+    var dest = [unzip.Parse(), fstream.Writer(TMP_DIR)];
+    download('http://codeload.github.com/kmalakoff/get-remote/zip/0.2.1', dest, function (err) {
+      assert.ok(!err);
+      var files = fs.readdirSync(path.join(TMP_DIR, 'get-remote-0.2.1'));
+      assert.ok(files.length > 1);
+      done();
+    });
+  });
+
+  it('should support promises', function (done) {
+    if (typeof Promise === 'undefined') return done(); // no promise support
+
+    var dest = [unzip.Parse(), fstream.Writer(TMP_DIR)];
+    download('http://codeload.github.com/kmalakoff/get-remote/zip/0.2.1', dest)
+      .then(function () {
+        var files = fs.readdirSync(path.join(TMP_DIR, 'get-remote-0.2.1'));
+        assert.ok(files.length > 1);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('should download with progress', function (done) {
+    var dest = [unzip.Parse(), fstream.Writer(TMP_DIR)];
+    var progressUpdates = [];
+
+    function createProgressStream(res) {
+      var progress = progressStream({
+        length: res.headers['content-length'] || 0,
+        drain: true,
+        speed: 20,
+      });
+      progress.on('progress', progressUpdates.push.bind(progressUpdates));
+      return progress;
+    }
+
+    download('http://codeload.github.com/kmalakoff/get-remote/zip/0.2.1', dest, { progress: createProgressStream }, function (err) {
+      assert.ok(!err);
+      var files = fs.readdirSync(path.join(TMP_DIR, 'get-remote-0.2.1'));
+      assert.ok(files.length > 1);
+      done();
+    });
+  });
+});
