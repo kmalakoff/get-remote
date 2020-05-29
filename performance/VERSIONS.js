7d54250d811faa3a31f75d9e86af506aca26ddfc
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-28 19:30:23 -0700

    Added performance tests

---
+++ performance/VERSIONS.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    version: 'latest',
+    download: require('..'),
+  },
+  {
+    version: '0.4.7',
+    download: require('get-remote-cb-0.4.7'),
+  },
+];
---
+++ performance/operations/tests.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var fs = require('fs');
+var rimraf = require('rimraf');
+var BenchmarkSuite = require('benchmark-suite');
+
+var TMP_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp'));
+
+module.exports = async function run({ download, version }) {
+  var suite = new BenchmarkSuite('download ' + version, 'Operations');
+
+  function testFn(highWaterMark, fn) {
+    return new Promise(function (resolve, reject) {
+      var filename = 'npm-' + highWaterMark + '.tgz';
+      download(
+        'https://registry.npmjs.org/npm/-/npm-6.14.5.tgz',
+        TMP_DIR,
+        { filename: filename, highWaterMark: highWaterMark, progress: fn, time: 1000 },
+        function (err) {
+          err ? reject(err) : resolve();
+        }
+      );
+    });
+  }
+
+  suite.add(`highWaterMark undefined`, function (fn) {
+    return testFn(undefined, fn);
+  });
+  suite.add(`highWaterMark 1024`, function (fn) {
+    return testFn(1024, fn);
+  });
+
+  suite.on('cycle', (results) => {
+    for (var key in results) console.log(`${results[key].name.padStart(8, ' ')}| ${suite.formatStats(results[key].stats)} - ${key}`);
+  });
+  suite.on('complete', function (results) {
+    console.log('-----Largest-----');
+    for (var key in results) console.log(`${results[key].name.padStart(8, ' ')}| ${suite.formatStats(results[key].stats)} - ${key}`);
+  });
+
+  console.log('----------' + suite.name + '----------');
+  try {
+    rimraf.sync(TMP_DIR);
+    fs.mkdirSync(TMP_DIR);
+  } catch (err) {}
+  await suite.run({ time: 1000 });
+  console.log('');
+};
