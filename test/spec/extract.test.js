12ea5f0819be63d0b729ed2f9f563d294a0d3712
Kevin Malakoff <kmalakoff@gmail.com>
2020-06-10 21:02:16 -0700

    Migrated to latest fast-extract

--- test/spec/extract.test.js
+++ test/spec/extract.test.js
@@ -4,6 +4,8 @@ var path = require('path');
 var rimraf = require('rimraf');
 var mkpath = require('mkpath');
 var semver = require('semver');
+var extract = require('fast-extract');
+var eos = require('end-of-stream');
 
 var get = require('../..');
 
@@ -13,29 +15,60 @@ try {
   if (lzmaNative) EXTRACT_TYPES.push('tar.xz');
 } catch (err) {}
 
-function addTests(extractType) {
-  describe(extractType, function () {
+function addTests(type) {
+  describe(type, function () {
     it('extract file', function (done) {
-      get('http://extractors.com/foo.' + extractType).extract(TMP_DIR, { strip: 1 }, function (err) {
+      get('http://extractors.com/foo.' + type).extract(TARGET, { strip: 1 }, function (err) {
         assert.ok(!err);
 
-        fs.readdir(TMP_DIR, function (err, files) {
+        fs.readdir(TARGET, function (err, files) {
           assert.ok(!err);
           assert.deepEqual(files.sort(), ['file.txt', 'link']);
-          assert.equal(fs.realpathSync(path.join(TMP_DIR, 'link')), path.join(TMP_DIR, 'file.txt'));
+          assert.equal(fs.realpathSync(path.join(TARGET, 'link')), path.join(TARGET, 'file.txt'));
           done();
         });
       });
     });
 
     it('extract file without type', function (done) {
-      get('http://extractors.com/foo-' + extractType).extract(TMP_DIR, { strip: 1, type: extractType }, function (err) {
+      get('http://extractors.com/foo-' + type).extract(TARGET, { strip: 1, type: type }, function (err) {
         assert.ok(!err);
 
-        fs.readdir(TMP_DIR, function (err, files) {
+        fs.readdir(TARGET, function (err, files) {
           assert.ok(!err);
           assert.deepEqual(files.sort(), ['file.txt', 'link']);
-          assert.equal(fs.realpathSync(path.join(TMP_DIR, 'link')), path.join(TMP_DIR, 'file.txt'));
+          assert.equal(fs.realpathSync(path.join(TARGET, 'link')), path.join(TARGET, 'file.txt'));
+          done();
+        });
+      });
+    });
+
+    it('extract file using stream', function (done) {
+      get('http://extractors.com/foo-' + type).stream(function (err, stream) {
+        assert.ok(!err);
+
+        var res = stream.pipe(extract.createWriteStream(TARGET, { strip: 1, type: type }));
+        eos(res, function (err) {
+          assert.ok(!err);
+
+          fs.readdir(TARGET, function (err, files) {
+            assert.ok(!err);
+            assert.deepEqual(files.sort(), ['file.txt', 'link']);
+            assert.equal(fs.realpathSync(path.join(TARGET, 'link')), path.join(TARGET, 'file.txt'));
+            done();
+          });
+        });
+      });
+    });
+
+    it('extract file using pipe', function (done) {
+      get('http://extractors.com/foo-' + type).pipe(extract.createWriteStream(TARGET, { strip: 1, type: type }), function (err) {
+        assert.ok(!err);
+
+        fs.readdir(TARGET, function (err, files) {
+          assert.ok(!err);
+          assert.deepEqual(files.sort(), ['file.txt', 'link']);
+          assert.equal(fs.realpathSync(path.join(TARGET, 'link')), path.join(TARGET, 'file.txt'));
           done();
         });
       });
@@ -44,6 +77,7 @@ function addTests(extractType) {
 }
 
 var TMP_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp'));
+var TARGET = path.resolve(path.join(TMP_DIR, 'target'));
 
 describe('extract', function () {
   if (semver.lt(process.versions.node, 'v0.10.0')) return;
@@ -53,9 +87,10 @@ describe('extract', function () {
   if (!require('timers').setImmediate) require('timers').setImmediate = require('next-tick');
   var nock = require('nock');
 
-  beforeEach(function (done) {
-    rimraf(TMP_DIR, function () {
-      mkpath(TMP_DIR, done);
+  beforeEach(function (callback) {
+    rimraf(TMP_DIR, function (err) {
+      if (err && err.code !== 'EEXIST') return callback(err);
+      mkpath(TMP_DIR, callback);
     });
   });
 
@@ -63,12 +98,12 @@ describe('extract', function () {
     var endpoint = nock('http://extractors.com').persist();
 
     for (var index = 0; index < EXTRACT_TYPES.length; index++) {
-      var extractType = EXTRACT_TYPES[index];
-      var fixturePath = path.join(__dirname, '..', 'data', 'fixture.' + extractType);
+      var type = EXTRACT_TYPES[index];
+      var fixturePath = path.join(__dirname, '..', 'data', 'fixture.' + type);
       endpoint
-        .get('/foo.' + extractType)
+        .get('/foo.' + type)
         .replyWithFile(200, fixturePath)
-        .get('/foo-' + extractType)
+        .get('/foo-' + type)
         .replyWithFile(200, fixturePath);
     }
   });
