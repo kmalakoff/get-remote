c280a5312e5cc4ed49fc1ee8ca4c91a2959a779f
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-18 00:20:45 -0700

    Format

--- test/lib/nock.js
+++ test/lib/nock.js
@@ -2,13 +2,11 @@ var semver = require('semver');
 
 if (semver.gte(process.versions.node, 'v10.0.0')) {
   module.exports = require('nock');
-}
-else if (semver.gte(process.versions.node, 'v0.10.0')) {
-  if (!global.WeakMap) global.WeakMap = require("weakmap-shim")
+} else if (semver.gte(process.versions.node, 'v0.10.0')) {
+  if (!global.WeakMap) global.WeakMap = require('weakmap-shim');
   if (!Object.assign) Object.assign = require('object-assign');
   if (!require('timers').setImmediate) require('timers').setImmediate = require('next-tick');
-  module.exports = require('nock-4');  
-}
-else {
-  module.exports = {};  
+  module.exports = require('nock-4');
+} else {
+  module.exports = {};
 }
