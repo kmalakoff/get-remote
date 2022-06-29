206a4f3af9da724a460b27ea715b5f24b85a0aa1
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-28 23:32:56 -0700

    Mode to just-extend

--- test/lib/nock.js
+++ test/lib/nock.js
@@ -4,7 +4,7 @@ if (semver.gte(process.versions.node, 'v10.0.0')) {
   module.exports = require('nock');
 } else if (semver.gte(process.versions.node, 'v0.10.0')) {
   if (!global.WeakMap) global.WeakMap = require('weakmap-shim');
-  if (!Object.assign) Object.assign = require('object-assign');
+  if (!Object.assign) Object.assign = require('just-extend');
   if (!require('timers').setImmediate) require('timers').setImmediate = require('next-tick');
   module.exports = require('nock-4');
 } else {
