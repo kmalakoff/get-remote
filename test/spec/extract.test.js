73196478bb3ba439a90176dd714b1dd5d0d5727d
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-19 17:30:28 -0700

    Got node 0.8 to work over https

--- test/spec/extract.test.js
+++ test/spec/extract.test.js
@@ -1,8 +1,6 @@
 var assert = require('assert');
-var path = require('path');
 var rimraf = require('rimraf');
 var mkpath = require('mkpath');
-var nock = require('../lib/nock');
 var extract = require('fast-extract');
 var eos = require('end-of-stream');
 
@@ -12,8 +10,12 @@ var validateFiles = require('../lib/validateFiles');
 var constants = require('../lib/constants');
 var TMP_DIR = constants.TMP_DIR;
 var TARGET = constants.TARGET;
+var URL = 'https://raw.githubusercontent.com/kmalakoff/get-remote/master';
 
+var major = +process.versions.node.split('.')[0];
+var minor = +process.versions.node.split('.')[1];
 var EXTRACT_TYPES = ['tar', 'tar.bz2', 'tar.gz', 'tgz', 'zip'];
+if (major === 0 && minor <= 8) EXTRACT_TYPES.pop(); //  TODO: fix zip on node 0.8
 try {
   var lzmaNative = require('require_optional')('lzma-native');
   if (lzmaNative) EXTRACT_TYPES.push('tar.xz');
@@ -23,7 +25,7 @@ function addTests(type) {
   describe(type, function () {
     it('extract file', function (done) {
       var options = { strip: 1 };
-      get('http://extractors.com/foo.' + type).extract(TARGET, options, function (err) {
+      get(URL + '/test/data/fixture.' + type).extract(TARGET, options, function (err) {
         assert.ok(!err);
 
         validateFiles(options, type, function (err) {
@@ -35,7 +37,7 @@ function addTests(type) {
 
     it('extract file without type', function (done) {
       var options = { strip: 1, type: type };
-      get('http://extractors.com/foo-' + type).extract(TARGET, options, function (err) {
+      get(URL + '/test/data/fixture-' + type).extract(TARGET, options, function (err) {
         assert.ok(!err);
 
         validateFiles(options, type, function (err) {
@@ -46,7 +48,7 @@ function addTests(type) {
     });
 
     it('extract file using stream', function (done) {
-      get('http://extractors.com/foo-' + type).stream(function (err, stream) {
+      get(URL + '/test/data/fixture-' + type).stream(function (err, stream) {
         assert.ok(!err);
 
         var options = { strip: 1, type: type };
@@ -64,7 +66,7 @@ function addTests(type) {
 
     it('extract file using pipe', function (done) {
       var options = { strip: 1, type: type };
-      get('http://extractors.com/foo-' + type).pipe(extract.createWriteStream(TARGET, options), function (err) {
+      get(URL + '/test/data/fixture-' + type).pipe(extract.createWriteStream(TARGET, options), function (err) {
         assert.ok(!err);
 
         validateFiles(options, type, function (err) {
@@ -77,27 +79,11 @@ function addTests(type) {
 }
 
 describe('extract', function () {
-  if (typeof nock !== 'function') return; // TODO: fix
-
   beforeEach(function (callback) {
     rimraf(TMP_DIR, function (err) {
       if (err && err.code !== 'EEXIST') return callback(err);
       mkpath(TMP_DIR, callback);
     });
-  });
-
-  before(function () {
-    var endpoint = nock('http://extractors.com').persist();
-
-    for (var index = 0; index < EXTRACT_TYPES.length; index++) {
-      var type = EXTRACT_TYPES[index];
-      var fixturePath = path.join(__dirname, '..', 'data', 'fixture.' + type);
-      endpoint
-        .get('/foo.' + type)
-        .replyWithFile(200, fixturePath)
-        .get('/foo-' + type)
-        .replyWithFile(200, fixturePath);
-    }
   });
 
   for (var index = 0; index < EXTRACT_TYPES.length; index++) addTests(EXTRACT_TYPES[index]);
