20c557d3fddbe2a26f9af8a447f37dbf4c837482
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-02 06:09:45 -0800

    Upgrade modules

--- dist/cjs/utils/extname.js
+++ dist/cjs/utils/extname.js
@@ -8,4 +8,4 @@ module.exports = function extname(fullPath, options) {
     if (!type && typeof options.extract === 'string') type = options.extract;
     return type;
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
