2e6edb0961eb7ead408620fa4ca67f0aa8a4ede9
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 22:18:00 -0800

    Upgrade modules

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import Response from './Response/index.js';
-export * from './types.js';
-export { default as Response } from './Response/index.js';
+import Response from './Response/index';
+export type * from './types';
+export { default as Response } from './Response/index';
 
 export default function getRemote(endpoint: string, options: object = {}) {
   return new Response(endpoint, options);
