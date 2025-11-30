cb05e5574e8ced2aba4c64964f8bd3279c53c631
Kevin Malakoff <kmalakoff@gmail.com>
2025-11-29 22:31:29 -0800

    Fixed promise and file-types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import Response from './Response/index.ts';
 import type { Options } from './types.ts';
 
+export type { FileTypeResult } from './lib/fileType.ts';
+export { default as fileType } from './lib/fileType.ts';
 export { default as Response } from './Response/index.ts';
+export { default as getBasename } from './sourceStats/basename.ts';
 export type * from './types.ts';
 
 export default function getRemote(endpoint: string, options: Options = {}) {
--- test/unit/download.test.ts
+++ test/unit/download.test.ts
@@ -1,19 +1,18 @@
 import assert from 'assert';
 import fs from 'fs';
+import get, { fileType, getBasename } from 'get-remote';
 import isTar from 'is-tar';
 import mkdirp from 'mkdirp-classic';
+import path from 'path';
 import Pinkie from 'pinkie-promise';
 import rimraf2 from 'rimraf2';
 
-// var contentDisposition = require('content-disposition');
-
-import get from 'get-remote';
-
-import { TARGET, TMP_DIR } from '../lib/constants.ts';
+import { DATA_DIR, TARGET, TMP_DIR } from '../lib/constants.ts';
 import streamToBuffer from '../lib/streamToBuffer.ts';
 import validateFiles from '../lib/validateFiles.ts';
 
 const URL = 'https://raw.githubusercontent.com/kmalakoff/get-remote/master';
+const GITHUB_ARCHIVE_URL = 'https://github.com/kmalakoff/get-remote/archive/refs/heads/master.zip';
 
 describe('download', () => {
   (() => {
@@ -51,8 +50,7 @@ describe('download', () => {
     });
   });
 
-  // TODO: debug on node 0.8
-  it.skip('get as promise', async () => {
+  it('get as promise', async () => {
     const stream = await get(`${URL}/test/data/fixture.tar`).stream();
     const buffer = await streamToBuffer(stream);
     assert.ok(isTar(buffer));
@@ -153,21 +151,25 @@ describe('download', () => {
     });
   });
 
-  it.skip('rename to valid filename', (done) => {
-    get(`${URL}/test/data/fix*ture.tar`).file(TARGET, (err?: Error) => {
-      if (err) {
-        done(err.message);
-        return;
-      }
-      fs.readdir(TARGET, (err, files) => {
-        if (err) {
-          done(err.message);
-          return;
-        }
-        assert.deepEqual(files.sort(), ['fix!ture.tar']);
-        done();
-      });
-    });
+  it('sanitize invalid filename characters', () => {
+    // Test POSIX invalid characters are replaced with '!'
+    // Note: ? is a query string delimiter in URLs, so it gets stripped not sanitized
+    assert.equal(getBasename(null, {}, 'http://example.com/foo*bar.tar'), 'foo!bar.tar');
+    assert.equal(getBasename(null, {}, 'http://example.com/foo<bar>.tar'), 'foo!bar!.tar');
+    assert.equal(getBasename(null, {}, 'http://example.com/foo:bar.tar'), 'foo!bar.tar');
+    assert.equal(getBasename(null, {}, 'http://example.com/foo"bar.tar'), 'foo!bar.tar');
+    assert.equal(getBasename(null, {}, 'http://example.com/foo|bar.tar'), 'foo!bar.tar');
+
+    // Test Windows reserved names are replaced
+    assert.equal(getBasename(null, {}, 'http://example.com/con'), '!');
+    assert.equal(getBasename(null, {}, 'http://example.com/prn'), '!');
+    assert.equal(getBasename(null, {}, 'http://example.com/aux'), '!');
+    assert.equal(getBasename(null, {}, 'http://example.com/nul'), '!');
+    assert.equal(getBasename(null, {}, 'http://example.com/com1'), '!');
+    assert.equal(getBasename(null, {}, 'http://example.com/lpt9'), '!');
+
+    // Test query strings are stripped before sanitization
+    assert.equal(getBasename(null, {}, 'http://example.com/file.tar?query=value'), 'file.tar');
   });
 
   it('follow redirects', (done) => {
@@ -221,37 +223,85 @@ describe('download', () => {
     });
   });
 
-  it.skip('handle content dispositon', (done) => {
-    get(`${URL}/test/data/fixture-tar`).file(TARGET, (err?: Error) => {
+  it('handle content disposition', (done) => {
+    // GitHub archive downloads send Content-Disposition header with filename
+    get(GITHUB_ARCHIVE_URL).stream((err, stream) => {
       if (err) {
         done(err.message);
         return;
       }
-      fs.readdir(TARGET, (err, files) => {
+      // Verify the content-disposition header was received and parsed
+      assert.ok(stream.headers['content-disposition'], 'Expected content-disposition header');
+      assert.ok(stream.headers['content-disposition'].indexOf('get-remote-master.zip') !== -1, 'Expected filename in content-disposition');
+
+      // Verify we can read the content as a zip
+      streamToBuffer(stream, (err, buffer) => {
         if (err) {
           done(err.message);
           return;
         }
-        assert.deepEqual(files.sort(), ['fixture-tar.tar']);
+        const type = fileType(buffer);
+        assert.ok(type && type.ext === 'zip', 'Expected zip content');
         done();
       });
     });
   });
 
-  it.skip('handle filename from file type', (done) => {
-    get('http://foo.bar/filetype').file(TARGET, (err?: Error) => {
+  it('detect file type from magic bytes', (done) => {
+    // Test that we can detect file types from content when URL has no extension
+    // Using GitHub archive URL which returns a zip file
+    get(GITHUB_ARCHIVE_URL).stream((err, stream) => {
       if (err) {
         done(err.message);
         return;
       }
-      fs.readdir(TARGET, (err, files) => {
+
+      streamToBuffer(stream, (err, buffer) => {
         if (err) {
           done(err.message);
           return;
         }
-        assert.deepEqual(files.sort(), ['filetype.tar']);
+
+        const result = fileType(buffer);
+        assert.ok(result, 'Expected file type to be detected');
+        assert.equal(result.ext, 'zip');
+        assert.equal(result.mime, 'application/zip');
         done();
       });
     });
+  });
+
+  it('detect various archive types from magic bytes', () => {
+    // Test file type detection with local test fixtures
+
+    // Test ZIP
+    const zipBuffer = fs.readFileSync(path.join(DATA_DIR, 'fixture.zip'));
+    const zipResult = fileType(zipBuffer);
+    assert.ok(zipResult, 'Expected zip to be detected');
+    assert.equal(zipResult.ext, 'zip');
+
+    // Test GZIP
+    const gzBuffer = fs.readFileSync(path.join(DATA_DIR, 'fixture.tar.gz'));
+    const gzResult = fileType(gzBuffer);
+    assert.ok(gzResult, 'Expected gzip to be detected');
+    assert.equal(gzResult.ext, 'gz');
+
+    // Test BZIP2
+    const bz2Buffer = fs.readFileSync(path.join(DATA_DIR, 'fixture.tar.bz2'));
+    const bz2Result = fileType(bz2Buffer);
+    assert.ok(bz2Result, 'Expected bzip2 to be detected');
+    assert.equal(bz2Result.ext, 'bz2');
+
+    // Test XZ
+    const xzBuffer = fs.readFileSync(path.join(DATA_DIR, 'fixture.tar.xz'));
+    const xzResult = fileType(xzBuffer);
+    assert.ok(xzResult, 'Expected xz to be detected');
+    assert.equal(xzResult.ext, 'xz');
+
+    // Test TAR (uncompressed)
+    const tarBuffer = fs.readFileSync(path.join(DATA_DIR, 'fixture.tar'));
+    const tarResult = fileType(tarBuffer);
+    assert.ok(tarResult, 'Expected tar to be detected');
+    assert.equal(tarResult.ext, 'tar');
   });
 });
