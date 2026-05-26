f83cabac33ce7ed945f7f2d2ca1e32f9749591db
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 08:56:16 -0700

    Fix broken types

--- src/Response/json.ts
+++ src/Response/json.ts
@@ -1,21 +1,23 @@
 import type { JSONCallback, JSONResponse } from '../types.ts';
+import type Response from './index.ts';
 
-function worker(callback) {
+function worker(this: Response, callback: JSONCallback) {
   return this.text((err, res) => {
     if (err) return callback(err);
+    if (!res) return callback(new Error('No response'));
 
     try {
-      res.body = JSON.parse(res.body);
-      return callback(null, res);
+      const body = JSON.parse(res.body);
+      return callback(undefined, { statusCode: res.statusCode, headers: res.headers, body } as JSONResponse);
     } catch (err) {
-      return callback(err);
+      return callback(err as Error);
     }
   });
 }
 
-export default function json(callback: JSONCallback): void;
-export default function json(): Promise<JSONResponse>;
-export default function json(callback?: JSONCallback): void | Promise<JSONResponse> {
+export default function json(this: Response, callback: JSONCallback): void;
+export default function json(this: Response): Promise<JSONResponse>;
+export default function json(this: Response, callback?: JSONCallback): void | Promise<JSONResponse> {
   if (typeof callback === 'function') return worker.call(this, callback);
-  return new Promise((resolve, reject) => worker.call(this, (err, res) => (err ? reject(err) : resolve(res))));
+  return new Promise((resolve, reject) => worker.call(this, (err, res) => (err ? reject(err) : resolve(res!))));
 }
