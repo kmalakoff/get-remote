4649151875b5677a68020385be57cc35a94412eb
Kevin Malakoff <kmalakoff@gmail.com>
2026-08-31 00:46:42 -0700

    Refactor tmpdir into compat.ts and add export tests

---
+++ test/exports/import.test.ts
@@ -0,0 +1,17 @@
+import assert from 'assert';
+import getRemote, { fileType, getBasename, Response } from 'get-remote';
+
+describe('exports .ts', () => {
+  it('default', () => {
+    assert.equal(typeof getRemote, 'function');
+  });
+  it('fileType', () => {
+    assert.equal(typeof fileType, 'function');
+  });
+  it('getBasename', () => {
+    assert.equal(typeof getBasename, 'function');
+  });
+  it('Response', () => {
+    assert.equal(typeof Response, 'function');
+  });
+});
